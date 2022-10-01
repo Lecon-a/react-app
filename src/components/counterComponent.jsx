b0516354b2d051d629b603ba0cb39c73af6237ae
Sunday P. Afolabi <spog4true@gmail.com>
2022-09-30 22:30:00 -0400

    Add components folder

---
+++ src/components/counterComponent.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+    state = {
+        value: this.props.value
+    };
+
+    // This is the second method for bind 'this' keyword to event handling function/method
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    // This is another way of binding 'this' keyword to event handling method using arrow function
+    handleIncrement = () => {
+        this.setState({ value: this.state.value + 1 })
+    }
+
+    render() { 
+
+        return (
+            <div>
+                { this.props.children }
+                <button
+                    onClick={this.handleIncrement}
+                    className={'btn btn-secondary btn-sm' + this.getMaginClass(2)}
+                >
+                    Increment
+                    <span className={this.getBadgeClass() + this.getMaginClass(1)}>{this.formatCount()}</span>
+                    Increment
+                </button>
+            </div>
+        );
+    }
+
+    getBadgeClass() {
+        let classes = "badge mb-1 bg-";
+        classes += this.state.value === 0 ? 'warning' : 'primary';
+        return classes;
+    } 
+
+    getMaginClass(m) {
+        let margin = ` m-${m} mb-0`;
+        return margin;
+    }
+
+    formatCount() {
+
+        let { value } = this.state;
+
+        return value === 0 ? "Zero" : value;
+
+    }
+}
+ 
+export default Counter;
